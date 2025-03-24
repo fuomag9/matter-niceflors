ed4c06d6243e4133af06f744a9c03823679b0353
fuomag9 <1580624+fuomag9@users.noreply.github.com>
2025-03-24 16:10:28 +0100

    Revert "removed home assistant stuff"
    This reverts commit 460f9d2a3495c042d6797404c7c90851586f1e6a.

--- cover.py
+++ cover.py
@@ -1,8 +1,30 @@
 from __future__ import annotations
 
 import logging
+import pigpio
 import time
 
+import voluptuous as vol
+
+from homeassistant.components.cover import (
+    PLATFORM_SCHEMA,
+    CoverEntity,
+    CoverEntityFeature,
+)
+from homeassistant.components.number import RestoreNumber
+from homeassistant.const import (
+    CONF_NAME,
+    CONF_UNIQUE_ID,
+    CONF_COVERS,
+    CONF_FRIENDLY_NAME,
+    EVENT_HOMEASSISTANT_STOP,
+    STATE_UNAVAILABLE,
+    STATE_UNKNOWN,
+)
+from homeassistant.core import HomeAssistant, ServiceCall
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 from .encode_tables import (
     NICE_FLOR_S_TABLE_ENCODE,
     NICE_FLOR_S_TABLE_KI,
@@ -28,9 +50,67 @@ BUTTON_ID_OPEN = 1
 BUTTON_ID_STOP = 2
 BUTTON_ID_CLOSE = 4
 
+COVER_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_SERIAL): cv.positive_int,
+        vol.Optional(CONF_UNIQUE_ID): cv.string,
+        vol.Optional(CONF_FRIENDLY_NAME): cv.string,
+    }
+)
 
-class RestoreNumber:
-    pass
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_GPIO): cv.positive_int,
+        vol.Optional(CONF_PIGPIO_HOST, default="localhost"): cv.string,
+        vol.Required(CONF_START_CODE): cv.positive_int,
+        vol.Required(CONF_COVERS): vol.Schema({cv.string: COVER_SCHEMA}),
+    }
+)
+
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    hass.data.setdefault(DOMAIN, {})
+
+    next_code = NextCodeEntity(config[CONF_START_CODE])
+    add_entities([next_code])
+
+    hub = NiceHub(
+        hass=hass,
+        pigpio_host=config[CONF_PIGPIO_HOST],
+        gpio=config[CONF_GPIO],
+        next_code=next_code,
+    )
+    hass.data[DOMAIN] = hub
+
+    devices = []
+    for dev_name, properties in config[CONF_COVERS].items():
+        devices.append(
+            NiceCover(
+                hub=hub,
+                friendly_name=properties.get(CONF_FRIENDLY_NAME),
+                unique_id=properties.get(CONF_NAME, dev_name),
+                serial=properties.get(CONF_SERIAL),
+            )
+        )
+
+    add_entities(devices)
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, lambda event: hub.cleanup())
+
+    hass.services.register(
+        DOMAIN,
+        "pair",
+        hub.pair,
+        vol.Schema(
+            {
+                vol.Required(CONF_SERIAL): cv.positive_int,
+            }
+        ),
+    )
 
 
 class NextCodeEntity(RestoreNumber):
@@ -43,11 +123,24 @@ class NextCodeEntity(RestoreNumber):
     def increase(self):
         self._attr_native_value = self._attr_native_value + 1
 
+    async def async_added_to_hass(self) -> None:
+        """Load the last known state when added to hass."""
+        await super().async_added_to_hass()
+        if (last_state := await self.async_get_last_state()) and (
+            last_number_data := await self.async_get_last_number_data()
+        ):
+            if last_state.state not in (STATE_UNKNOWN, STATE_UNAVAILABLE):
+                self._attr_native_value = last_number_data.native_value
+
 
 class RFDevice:
     def __init__(
         self,
+        gpio: int,
+        pi: pigpio,
     ) -> None:
+        self._pi = pi
+        self._gpio = gpio
         #self.tx_pulse_short = 250  # Halved values
         #self.tx_pulse_long = 500   # Halved values
         #self.tx_pulse_sync = 750   # Halved values
@@ -83,14 +176,77 @@ class RFDevice:
 
         self._pi.wave_delete(wave)
 
+    def tx_l0(self):
+        return [
+            pigpio.pulse(self._gpio, GPIO_NONE, self.tx_pulse_short),
+            pigpio.pulse(GPIO_NONE, self._gpio, self.tx_pulse_long),
+        ]
+
+    def tx_l1(self):
+        return [
+            pigpio.pulse(self._gpio, GPIO_NONE, self.tx_pulse_long),
+            pigpio.pulse(GPIO_NONE, self._gpio, self.tx_pulse_short),
+        ]
+
+    def tx_sync(self):
+        return [
+            pigpio.pulse(self._gpio, GPIO_NONE, self.tx_pulse_sync),
+            pigpio.pulse(GPIO_NONE, self._gpio, self.tx_pulse_sync),
+        ]
+
+    def tx_gap(self):
+        return [
+            pigpio.pulse(GPIO_NONE, self._gpio, self.tx_pulse_gap),
+        ]
+
+
+class PigpioNotConnected(BaseException):
+    """"""
+
 
 class NiceHub:
     def __init__(
         self,
+        hass: HomeAssistant,
+        pigpio_host: str | None,
+        gpio: int,
         next_code: NextCodeEntity,
     ) -> None:
+        self._gpio = gpio
         self._next_code = next_code
         self._lock = Lock()
+
+        self._pi = pigpio.pi(pigpio_host)
+        _LOGGER.info("Connecting to pigpio on %s, please wait...", pigpio_host)
+
+        i = 0
+        while not self._pi.connected:
+            i += 1
+            _LOGGER.info("...")
+            time.sleep(1)
+            if i > 9:
+                raise PigpioNotConnected()
+        self._pi.set_mode(gpio, pigpio.OUTPUT)
+
+        rfdevice = RFDevice(gpio=1 << gpio, pi=self._pi)
+        self._rfdevice = rfdevice
+
+    def cleanup(self):
+        with self._lock:
+            self._pi.stop()
+
+    def pair(self, service_call: ServiceCall):
+        with self._lock:
+            button_id = BUTTON_ID_STOP
+            code = int(self._next_code.native_value)
+            serial = service_call.data[CONF_SERIAL]
+
+            _LOGGER.info("Starting pairing of %s... Wait 5 seconds.", hex(serial))
+
+            for _ in range(1, 10):
+                self._send_repeated(serial, button_id, code)
+
+            _LOGGER.info("Entered pairing mode for %s.", hex(serial))
 
     def send(self, serial: int, button_id: int):
         with self._lock:
@@ -144,4 +300,38 @@ class NiceHub:
         encoded = encoded >> 4
 
         return encoded
+
+
+class NiceCover(CoverEntity):
+    def __init__(
+        self,
+        hub: NiceHub,
+        friendly_name: str,
+        unique_id: str,
+        serial: int,
+    ):
+        super().__init__()
+        self._hub = hub
+        self._serial = serial
+
+        self._attr_assumed_state = True
+        self._attr_name = friendly_name
+        self._attr_unique_id = unique_id
+        self._attr_supported_features = (
+            CoverEntityFeature.OPEN | CoverEntityFeature.CLOSE | CoverEntityFeature.STOP
+        )
+        self._attr_device_class = DEVICE_CLASS
+        self._attr_current_cover_position = 50
+        self._attr_is_closed = False
+        self._attr_is_opening = False
+        self._attr_should_poll = False
+
+    def open_cover(self, **kwargs):
+        self._hub.send(self._serial, BUTTON_ID_OPEN)
+
+    def close_cover(self, **kwargs):
+        self._hub.send(self._serial, BUTTON_ID_CLOSE)
+
+    def stop_cover(self, **kwargs) -> None:
+        self._hub.send(self._serial, BUTTON_ID_STOP)
 
